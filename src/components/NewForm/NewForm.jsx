6194e2a11e26cb5c3740fbd2b26f9c2218a639b3
Nate Morgan <nathanielrmorgan@gmail.com>
2022-12-31 12:48:57 -0500

    update form based on state

--- src/components/NewForm/NewForm.jsx
+++ src/components/NewForm/NewForm.jsx
@@ -1,19 +1,33 @@
+import { useState } from "react"
+
 const NewForm = (props) => {
+
+  const [formData,setFormData] = useState(
+    {
+      firstName:'',
+      lastName:'',
+      phoneNumber:''
+    }
+  )
 
   const handleSubmit = (e) => {
     e.preventDefault()
   }
 
+  const handleChange = (e) => {
+    setFormData({...formData,[e.target.name]:e.target.value})
+  }
+
   return ( 
     <form onSubmit={handleSubmit}>
       <label htmlFor="lastName"> Last Name: </label>
-      <input type="text" name="lastName" />
+      <input type="text" name="lastName" value={formData.lastName} onChange={handleChange}/>
       <br />
       <label htmlFor="firstName"> First Name: </label>
-      <input type="text" name="firstName" />
+      <input type="text" name="firstName" value={formData.firstName} onChange={handleChange}/>
       <br />
       <label htmlFor="phoneNumber"> Phone Number: </label>
-      <input type="text" name="phoneNumber" />
+      <input type="text" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange}/>
       <br />
       <button type="submit"> Add Contact </button>
     </form>
