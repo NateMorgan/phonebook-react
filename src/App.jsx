209e218bbe6a1a663bdbe81c144e03d3f9fbf9b2
Nate Morgan <nathanielrmorgan@gmail.com>
2022-12-31 13:48:04 -0500

    dynamic search of the phonebook

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,49 @@ import SearchBar from './components/SearchBar/SearchBar';
 
 function App() {
 
+  
   const [phonebook,setPhonebook] = useState([
     {
       firstName:'Nathaniel',
       lastName:'Morgan',
       phoneNumber: '123-456-7890'
+    },
+    {
+      firstName:'Bailey',
+      lastName:'Stine',
+      phoneNumber: '098-765-4321'
+    },
+    {
+      firstName:'Jess',
+      lastName:'Landis',
+      phoneNumber: '068-725-4391'
+    },
+    {
+      firstName:'Damian',
+      lastName:'Bdzyra',
+      phoneNumber: '168-724-5390'
     }
   ])
 
+  let [search,setSearch] = useState('')
+  
   const addContact = (newContact) => {
     setPhonebook([...phonebook,newContact])
   }
 
+  const changeSearch = (change) => {
+    setSearch(change)
+  }
+
+  let filteredPhonebook = phonebook.filter( contact => {
+    return contact.firstName.toLowerCase().includes(search.toLowerCase()) || contact.lastName.toLowerCase().includes(search.toLowerCase())
+  })
+  
   return (
     <>
       <Header/>
-      <SearchBar/>
-      <Phonebook phonebook={phonebook}/>
+      <SearchBar changeSearch={changeSearch}/>
+      <Phonebook phonebook={filteredPhonebook}/>
       <NewForm addContact={addContact}/>
     </>
   );
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,14 @@
 const SearchBar = (props) => {
+
+  const handleChange = (e) => {
+    props.changeSearch(e.target.value)
+  }
+
   return ( 
     <input 
       type="text" name="search" id="searchBar"
       placeholder="Search Contacts"
+      onChange={handleChange}
     />
   );
 }
