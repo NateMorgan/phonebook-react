f8600f36fd8c62077c15c28c2f24bbe55295d918
Nate Morgan <nathanielrmorgan@gmail.com>
2022-12-31 12:09:57 -0500

    inital static setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return ( 
+    <div className="App">
+      <header className="App-header">
+        <h1>React Phonebook</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Phonebook/Phonebook.jsx
@@ -0,0 +1,22 @@
+const Phonebook = (props) => {
+  return ( 
+    <table>
+      <thead>
+        <tr>
+          <th> Last Name </th>
+          <th> First Name </th>
+          <th> Phone Number </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td> Morgan </td>
+          <td> Nathaniel </td>
+          <td> 123-456-7890 </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default Phonebook;
